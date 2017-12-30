188d5dda24459bb3b0e75b78b68b78be6dc324ef
Malay Agarwal <malay.agarwal261016@outlook.com>
2017-12-30 21:25:25 +0530

    Add functionality - links, file, email

--- Collect.py
+++ Collect.py
@@ -1,4 +1,9 @@
 import feedparser
+import smtplib
+from email.mime.multipart import  MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
 
 #TODO - Get a list of links from the websites
 #TODO - Compile them into a text file which has links that can be clicked (.md maybe)
@@ -9,16 +14,66 @@ urls = ["http://planetpython.org/rss20.xml", "https://www.reddit.com/r/Python/.r
 def compileLinks(urls):
     links = {}
     for url in urls:
-        pass
+        feed = feedparser.parse(url)
+        title = feed['feed']['title']
+        links[title] = {}
+        i = 1
+        for entry in feed['entries']:
+            links[title][i] = {}
+            links[title][i]['title'] = entry['title']
+            links[title][i]['link'] = entry['link']
+            i += 1
 
-def compileFile():
-    pass
+    return links
 
-def mail():
-    pass
+def compileFile(links):
+    with open('links.txt', 'w') as file:
+        for title, items in links.items():
+            file.write(f'{title}\n')
+            for number, details in items.items():
+                headline = details['title']
+                link = details['link']
+                try:
+                    file.write(f"\t{number}. {headline} - {link}\n")
+                except:
+                    continue
+            file.write("\n")
+
+        return file.name
+
+def mail(filename):
+    sender = 'rssfeedlinks1@gmail.com'
+    recipient = 'malay.agarwal261016@outlook.com'
+    password = 'MakingLifeEasier'
+    subject = 'Your Python headlines for today!'
+
+    message = MIMEMultipart()
+    message['From'] = sender
+    message['To'] = recipient
+    message['Subject'] = subject
+
+    attachment = open(filename, 'rb')
+
+    attachmentPart = MIMEBase('application', 'octet-string')
+    attachmentPart.set_payload(attachment.read())
+    encoders.encode_base64(attachmentPart)
+    attachmentPart.add_header("News", 'attachment; filename = ' + filename)
+
+    message.attach(attachmentPart)
+    message = message.as_string()
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(sender, password)
+
+    server.sendmail(sender, recipient, message)
+    server.quit()
+
 
 def main():
     links = compileLinks(urls)
+    filename = compileFile(links)
+    mail(filename)
 
 if __name__ == '__main__':
     main()
