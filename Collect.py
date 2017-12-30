464ca5307f0d33ebdde22a20e2667d313a883ffe
Malay Agarwal <malay.agarwal261016@outlook.com>
2017-12-30 21:43:31 +0530

    TODO completed

--- Collect.py
+++ Collect.py
@@ -4,11 +4,6 @@ from email.mime.multipart import  MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 
-
-#TODO - Get a list of links from the websites
-#TODO - Compile them into a text file which has links that can be clicked (.md maybe)
-#TODO - Email the file as an attachment to myself
-
 urls = ["http://planetpython.org/rss20.xml", "https://www.reddit.com/r/Python/.rss"]
 
 def compileLinks(urls):
