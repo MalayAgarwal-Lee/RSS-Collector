f8524dac58f8ae2104e1b069e61a6aea4bec763b
Malay Agarwal <malay.agarwal261016@outlook.com>
2018-01-25 13:59:12 +0530

    Fixes
    Avoid special characters + Remove Machine Learning on Medium tag + Add Towards Data Science on Medium

--- Collect.py
+++ Collect.py
@@ -6,10 +6,10 @@ from email.mime.base import MIMEBase
 from email import encoders
 
 #the urls of the blogs I am obtaining the news from
-urls = ["http://planetpython.org/rss20.xml", "https://www.reddit.com/r/Python/.rss", "http://machinelearningmastery.com/blog/feed/", "http://news.mit.edu/rss/topic/artificial-intelligence2", "http://mlweekly.com/issues.rss", "https://medium.com/feed/tag/machine-learning"]
+urls = ["http://planetpython.org/rss20.xml", "https://www.reddit.com/r/Python/.rss", "http://machinelearningmastery.com/blog/feed/", "http://news.mit.edu/rss/topic/artificial-intelligence2", "http://mlweekly.com/issues.rss", "https://towardsdatascience.com/feed/"]
 
 def avoidUrls():
-    avoid = ['https://www.codementor.io/edmondatto/build-a-command-line-application-that-consumes-a-public-api-here-s-how-f7hxbuxm2','http://pyarab.com/2017/12/python-send-email.html', 'https://www.numfocus.org/blog/communicating-feedback-as-a-service-notes-from-the-disc-unconference/', 'http://feeds.doughellmann.com/~r/doughellmann/python/~3/_tlxn_DttE0/', 'https://medium.com/@yanastrokova/%D0%BD%D0%B5-%D0%B7%D0%BD%D0%B0%D0%BD%D0%B8%D0%B5-%D0%BA%D0%BB%D0%B8%D0%B5%D0%BD%D1%82%D0%B0-%D0%BD%D0%B5-%D0%BE%D1%81%D0%B2%D0%BE%D0%B1%D0%BE%D0%B6%D0%B4%D0%B0%D0%B5%D1%82-%D0%BE%D1%82-%D0%BE%D1%82%D0%B2%D0%B5%D1%82%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D0%B8-2-cbb40d3e9346?source=rss------machine_learning-5']
+    avoid = ['http://feeds.doughellmann.com/~r/doughellmann/python/~3/_tlxn_DttE0/', 'https://medium.com/@yanastrokova/%D0%BD%D0%B5-%D0%B7%D0%BD%D0%B0%D0%BD%D0%B8%D0%B5-%D0%BA%D0%BB%D0%B8%D0%B5%D0%BD%D1%82%D0%B0-%D0%BD%D0%B5-%D0%BE%D1%81%D0%B2%D0%BE%D0%B1%D0%BE%D0%B6%D0%B4%D0%B0%D0%B5%D1%82-%D0%BE%D1%82-%D0%BE%D1%82%D0%B2%D0%B5%D1%82%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D0%B8-2-cbb40d3e9346?source=rss------machine_learning-5',]
 
     for url in avoid:
         yield url
@@ -55,15 +55,18 @@ def compileFile(links):
             avoid = avoidUrls()
             for number, details in items.items():
 
-                #I am avoiding these links
-                #the first one has a special character in the headline
-                #the second is in Arabic
-                #they cannot be handled by .write() method
-                if details['link'] not in avoid:
+                headline = details['title']
+                #checking for Em dash
+                #cannot be handled by write(), so being replaced
+                emDash = '\u2014'
+                if emDash in details['title']:
+                    headline = headline.replace(emDash, '-', headline.count(emDash))
 
+                #avoiding some problematic urls
+                if details['link'] not in avoid:
                     #creating a proper list object
-                    string =  soupObject.new_string(details['title'])
-                    newTag = soupObject.new_tag("li", id = f'{number}-{title}')
+                    string =  soupObject.new_string(headline)
+                    newTag = soupObject.new_tag("li")
                     newTag.append(soupObject.new_tag('a', href = details['link']))
                     newTag.a.append(string)
                     soupObject.find('ul', id = f'links from {title}').append(newTag)
